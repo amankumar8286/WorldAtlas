6894d6b082efcf11991bdab9f09bf9e2769f12d1
amankumar <amankumar8286>
2025-08-12 14:56:13 +0530

    contact page update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,20 +7,23 @@ const handleFormSubmit = (formData) => {
 };
 
 
-    return <header>
+    return <header className="">
         <section className="section-contact">
         <h2 className="container-title"></h2>
         <div className="contact-wrapper container">
         <form action={handleFormSubmit}>
             <input type="text" required autoComplete="false"placeholder="Enter your name"name="username" />
 
-            <input type="mail"className="form-control" required autoComplete="false"placeholder="Enter your email"name="email" />
-
-            <textarea name="message" className="form-control" rows="10"  id="" required autoComplete="false"></textarea>
+            <input type="mail"className="form-control" required autoComplete="false"placeholder="Enter your email" name="email" />
+{/* <textarea name="type your message here" id="" required autoComplete="false"></textarea> */}
+            <textarea name="message" className="form-control" rows="10"  id="" placeholder="Write here your message in description" required autoComplete="false">Write here your message</textarea>
             <button type="submit" value="send">Send</button>
+            
         </form>
+
         </div>
     </section>
+    
     </header>
     
 };
